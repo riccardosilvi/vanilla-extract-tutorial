224cfcfd7e2d5c0dbc1e340f70822f7cc6f5952d
Riccardo Silvi <r.silvi@young.business>
2022-12-29 11:20:43 +0100

    add wrapper styles

--- src/components/app.css.ts
+++ src/components/app.css.ts
@@ -1,4 +1,14 @@
 import { style } from "@vanilla-extract/css"
 import { vars } from "../styles/theme.css"
 
-export const wrapper = style({})
+export const wrapper = style({
+  fontFamily: vars.font.body,
+  backgroundColor: vars.colors.background,
+  color: vars.colors.body,
+  height: `100vh`,
+  width: `100%`,
+  display: `flex`,
+  justifyContent: `center`,
+  alignItems: `center`,
+  fontSize: vars.fontSize.md,
+})
